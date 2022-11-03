543eddb6475e4799ecd8c75a0cfcd0833f1e6209
jhony2022 <thecapoflash@hotmail.com>
2022-11-03 01:52:47 -0300

    adding second carrousell of her baby-photos and the example video

---
+++ codigo.js
@@ -0,0 +1,13 @@
+
+//alert('hi');
+//let ingresar = parseInt(prompt(`Password`));
+
+
+//if( ingresar == 'sari2022'){
+    //console.log('joined');
+//}
+//while (ingresar != 'sari2022'){
+    //ingresar = prompt(`Password`);
+//}
+
+    
